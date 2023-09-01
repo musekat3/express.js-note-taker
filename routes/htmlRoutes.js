457ba941ff1143b50974fffcea09dbbf7294399a
Kathy Hyde <musekat3@gmail.com>
2023-08-31 22:23:59 -0700

    Updates

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,11 +2,20 @@ const path = require('path');
 
 module.exports = (app) => {
 
-  app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-  });
+  // app.get('/notes', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../public/notes.html'));
+  // });
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile('index.html', {root: 'public'});
+  });
+
+  app.get('/notes', (req, res) => {
+    res.sendFile('index.html', {root: 'public'});
   })
 };
+
+//  app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname, '../public/index.html'));
+//   })
+// };
